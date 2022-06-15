761afefb104cd62786b7b6c182c15d6ba80174e7
zaurmag411@gmail.com <guKGyVmC7o>
2022-06-15 23:06:55 +0300

    Upd: Dependencies and fix sass code

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,24 +1,22 @@
-let path = require('./path/path.js');
+let path = require('./path/path.js')
+const sass = require('gulp-sass')(require('sass'))
 
 module.exports = function () {
-	// eslint-disable-next-line consistent-return
-	$.gulp.task('style:build', (done) => {
+	$.gulp.task('style:build', () => {
 		return $.gulp.src(path.path.src.style)
 			.pipe($.plugins.plumber())
-			.pipe($.plugins.if(!$.yargs.minifyCss, $.plugins.sourcemaps.init({largeFile: true})))
-			.pipe($.plugins.sass())
+			.pipe($.plugins.if(!$.yargs.minifyCss, $.plugins.sourcemaps.init({ largeFile: true })))
+			.pipe(sass().on('error', sass.logError))
 			.pipe($.plugins.pxtorem())
 			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.csso()))
-			.pipe($.plugins.autoprefixer({
+			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.autoprefixer({
 				overrideBrowserslist: ['last 2 versions'],
-				cascade: false,
-			}))
-			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.rename({suffix: '.min'}), $.plugins.sourcemaps.write('.')))
+				cascade: false
+			})))
+			.pipe($.plugins.if(!$.yargs.minifyCss, $.plugins.sourcemaps.write('.')))
 			.pipe($.gulp.dest(path.path.build.style))
 			.pipe($.browserSync.reload({
-				stream: true,
-			}));
-		// eslint-disable-next-line no-unreachable
-		done();
-	});
-};
+				stream: true
+			}))
+	})
+}
