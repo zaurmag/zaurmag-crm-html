b47884e81b57cf467ae216cf6e4f57738be6cb21
Заур <zaurmag411@gmail.com>
2022-09-06 12:10:43 +0300

    Upd: Project list table
    And other: pagination, select in filter, button Add record...

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,11 +1,15 @@
 import Quill from 'quill'
 
-new Quill('#editor', {
-    "modules": {
-        "toolbar": [
-            ["bold", "italic", "underline", "strike", "link", "image", "blockquote", "code", {"list": "bullet"}]
-        ]
-    },
-    placeholder: 'Расскажите немного о себе...',
-    theme: 'snow'
-})
+const $editor = document.querySelector('#editor')
+
+if ($editor) {
+    new Quill('#editor', {
+        "modules": {
+            "toolbar": [
+                ["bold", "italic", "underline", "strike", "link", "image", "blockquote", "code", {"list": "bullet"}]
+            ]
+        },
+        placeholder: 'Расскажите немного о себе...',
+        theme: 'snow'
+    })
+}
